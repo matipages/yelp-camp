f280782cbbb502ed6fa148fe6fcf2fb2c775f9b4
matipages <pages.matiasm@gmail.com>
2024-01-12 14:36:17 -0300

    se agregan archivos dentro de la carpeta seeds

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+  console.log("Database Conected")
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
+
+
+//-----------
 const app = express();
 
 app.set('view engine', 'ejs');
