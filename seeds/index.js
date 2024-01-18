96c5c7df0fc354aa4933e433c0ac8704359c0814
matipages <pages.matiasm@gmail.com>
2024-01-18 15:52:52 -0300

    se agrega boostrap y se crean layouts y partials

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random() * 20) +10;
         const camp = new Campground({
             location: `${cities[random].city}, ${cities[random].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/random/?camping',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis laborum pariatur dolorem cupiditate ab praesentium debitis explicabo dicta vero illum, sunt dolore unde officia ut molestiae fugit mollitia? Maiores, voluptatem.',
+            price: price
         })
         await camp.save();
     }
